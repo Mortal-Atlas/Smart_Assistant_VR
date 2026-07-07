fdb8e1cef9608b3cf207f8fd77db7d146bd94724
Jacob Zander Gino <jacobzgino@gmail.com>
2026-07-06 22:32:57 -0400

    fully working button

--- Assets/AR/Scripts/ArtemisButton.cs
+++ Assets/AR/Scripts/ArtemisButton.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using M2MqttUnity;
 using uPLibrary.Networking.M2Mqtt.Messages;
 using System.Text;
-
+using UnityEngine.InputSystem;
 public class ArtemisButton : MonoBehaviour
 {
     [Tooltip("Drag your MQTT_manager object here")]
@@ -28,9 +28,52 @@ public class ArtemisButton : MonoBehaviour
 
    void Update()
     {
-       if(Input.GetKeyDown(KeyCode.Space)) // For testing in the Unity Editor
-       {
-           ToggleSwitch();
-       }
+       if (Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame)
+    {
+        ToggleSwitch();
     }
+    }
+    void OnApplicationPause(bool pauseStatus)
+{
+    if (!pauseStatus) // If pauseStatus is false, the app has just RESUMED
+    {
+        Debug.Log("Headset resumed - Checking MQTT connection...");
+        
+        // If connected, disconnect cleanly first to avoid zombie sockets
+        if (mqttManager.client != null && mqttManager.client.IsConnected)
+        {
+            mqttManager.client.Disconnect();
+        }
+
+        // Re-initiate connection
+        mqttManager.Connect(); 
+    }
+}
+// Inside your reconnection method (triggered by OnApplicationPause)
+public void ReconnectMQTT()
+{
+    // Ensure the client is disconnected first
+    if (mqttManager.client != null && mqttManager.client.IsConnected)
+    {
+        mqttManager.client.Disconnect();
+    }
+
+    // Force connect with cleanSession = false
+    // You may need to provide the full list of arguments if it's not exposing it
+    // The cleanSession parameter is typically the 9th argument in M2Mqtt
+    mqttManager.client.Connect(
+        "UnityQuestClient", // ClientID
+        "unity_user",    // Username
+        "12345",    // Password
+        false,              // Will Retain
+        0,                  // Will QoS
+        false,              // Will Retain
+        null,               // Will Topic
+        null,               // Will Message
+        false,              // <--- THIS IS THE CLEAN SESSION FLAG
+        60                  // Keep Alive
+    );
+    
+    Debug.Log("MQTT Reconnected with Persistent Session.");
+}
 }
